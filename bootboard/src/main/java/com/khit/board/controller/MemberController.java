44008a2dba434be25d241448ee7cfb3949d7dff7
LEEJEHEON717 <leejehun0717@naver.com>
2024-01-15 20:27:43 +0900

    bootboard

--- bootboard/src/main/java/com/khit/board/controller/MemberController.java
+++ bootboard/src/main/java/com/khit/board/controller/MemberController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.khit.board.dto.MemberDTO;
+import com.khit.board.entity.Member;
 import com.khit.board.service.MemberService;
 
 import jakarta.servlet.http.HttpSession;
@@ -21,27 +23,26 @@ public class MemberController {
 	
 	private final MemberService memberService;
 	
-	
 	@GetMapping("/member/join")
 	public String joinForm() {
-		return "/member/join"; //join.html
+		return "/member/join";  //join.html
 	}
 	
-	//회원가입
+	//회원 가입
 	@PostMapping("/member/join")
 	public String join(@ModelAttribute MemberDTO memberDTO) {
-		System.out.println("memberDTO: " + memberDTO);
+		System.out.println("memberDTO:" + memberDTO);
 		memberService.save(memberDTO);
 		return "redirect:/member/login"; 
 	}
 	
-	//회원목록
+	//회원 목록
 	@GetMapping("/member/list")
 	public String getList(Model model) {
-		//db에서 꺼내서 memberDTO로 반환할 것
+		//db에서 꺼내와서 memberDTO로 반환할 것
 		List<MemberDTO> memberDTOList = memberService.findAll();
 		model.addAttribute("memberList", memberDTOList);
-		return "/member/list"; //list.html
+		return "/member/list";  //list.html
 	}
 	
 	//회원 상세보기
@@ -49,23 +50,22 @@ public class MemberController {
 	@GetMapping("/member/{id}")
 	public String getMember(@PathVariable Long id,
 			Model model) {
-		
 		MemberDTO memberDTO = memberService.findById(id);
 		model.addAttribute("member", memberDTO);
-		return "/member/detail"; //datail.html	
+		return "/member/detail";  //detail.html
 	}
 	
 	//회원 삭제하기
 	@GetMapping("/member/delete/{id}")
 	public String deleteMember(@PathVariable Long id) {
 		memberService.deleteById(id);
-		return "redirect:/member/list";	
+		return "redirect:/member/list";
 	}
 	
 	//로그인 페이지 요청
 	@GetMapping("/member/login")
 	public String loginForm() {
-		return "/member/login";	//login.html
+		return "/member/login";  //login.html
 	}
 	
 	//로그인 처리
@@ -74,14 +74,14 @@ public class MemberController {
 			HttpSession session, Model model) {
 		MemberDTO loginMember = memberService.login(memberDTO);
 		//로그인한 결과(객체가 있으면 로그인됨, 없으면 다시 로그인폼)
-		if(loginMember !=null) {
+		if(loginMember != null) {
 			//세션 발급(이메일)
 			session.setAttribute("sessionEmail", loginMember.getMemberEmail());
-			return "/main"; //main.html			
+			return "main";  //main.html
 		}else {
 			String error = "아이디나 비밀번호를 확인해 주세요";
 			model.addAttribute("error", error);
-			return "/member/login"; //login.html
+			return "/member/login";  //login.html
 		}
 	}
 	
@@ -101,10 +101,11 @@ public class MemberController {
 		return "/member/update"; //update.html
 	}
 	
-	//수정 처리
+	//수정 처리 - 상세보기
 	@PostMapping("/member/update")
 	public String update(MemberDTO memberDTO) {
 		memberService.update(memberDTO);
 		return "redirect:/member/" + memberDTO.getId();
 	}
+	
 }
